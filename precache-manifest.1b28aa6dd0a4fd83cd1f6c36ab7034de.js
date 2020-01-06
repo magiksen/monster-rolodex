9d72c5edcb8627861a0d975d0a4893f801af49be
magiksen <elemejore@gmail.com>
2020-01-06 11:53:27 -0400

    Updates

---
+++ precache-manifest.1b28aa6dd0a4fd83cd1f6c36ab7034de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "121943af889394fe33c16b734cba58f8",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "69fbd2998858bd4b3f87",
+    "url": "/monster-rolodex/static/css/main.82950751.chunk.css"
+  },
+  {
+    "revision": "8d82078e3494a7266b17",
+    "url": "/monster-rolodex/static/js/2.352aa1eb.chunk.js"
+  },
+  {
+    "revision": "69fbd2998858bd4b3f87",
+    "url": "/monster-rolodex/static/js/main.a92e4ff3.chunk.js"
+  },
+  {
+    "revision": "1f1fdb189afd82863836",
+    "url": "/monster-rolodex/static/js/runtime~main.80659f76.js"
+  }
+]);
